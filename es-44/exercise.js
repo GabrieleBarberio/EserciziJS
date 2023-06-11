e5729b673abfaae215a4c140657a477b3c1baa4c
Gabriele Barberio <gabriele.barberio@gmail.com>
2023-06-11 13:11:30 +0200

    added exercise

---
+++ es-44/exercise.js
@@ -0,0 +1,29 @@
+
+
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+const internalMemory = {
+
+  save:(key, value) => {
+      if (typeof key !== "string") throw new Error("key must be a valid string")
+      if (value === undefined) throw new Error ("value cannot be undefined")
+      localStorage.setItem(key, JSON.stringify(value))
+  },
+  get:(key) => {
+      if (typeof key !== "string") throw new Error("key must be a valid string")
+      return JSON.parse(localStorage.getItem(key))
+  },
+  remove:(key) => {
+      if (typeof key !== "string") throw new Error("key must be a valid string")
+      localStorage.removeItem(key)
+  },
+  clear:() => {
+      localStorage.clear()
+  }
+}
+internalMemory.save('user', user)
+
+console.log(internalMemory.get('user'));
